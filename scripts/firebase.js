7d6a63ce01c493591a1d583d1d766d65e95bdd48
bdziaul <ziauludpn@gmail.com>
2025-11-10 20:08:51 +0600

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,1 +1,21 @@
+// scripts/firebase.js
+// Firebase Configuration (UDPN Chat)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjt8sx0NSrSju7JnGq36RERZNY8uamScc",
+  authDomain: "udpnchat.firebaseapp.com",
+  databaseURL: "https://udpnchat-default-rtdb.firebaseio.com",
+  projectId: "udpnchat",
+  storageBucket: "udpnchat.firebasestorage.app",
+  messagingSenderId: "1006055561503",
+  appId: "1:1006055561503:web:b543dc4ac8f2e6cbf1b82a",
+  measurementId: "G-W6FGT46QWT"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firebase references
+const auth = firebase.auth();
+const database = firebase.database();
 
